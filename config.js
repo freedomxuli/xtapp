80d5b9f23300afc5371061e96a529f3fef08502c
yangyang <413193907@qq.com>
2018-06-04 23:04:18 +0800

    Signed-off-by: yangyang <413193907@qq.com>

--- config.js
+++ config.js
@@ -140,6 +140,7 @@ var config = {
 		getNetworkList: 'network.list', //线下网络
 		getAccountDeposit: 'accountCheck.deposit', //余额对账
 		getAccountTickets: 'accountCheck.tickets', //开票对账
-		deliver:'member.stop.deliver'//订单物流提前终止发货
+		deliver:'member.stop.deliver',//订单物流提前终止发货
+	    buyAndApply:'buy.and.apply'//求购供应-登记
 	}
 }
